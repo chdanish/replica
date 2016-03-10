813f95180c230a79a26376fca33145aa6aae1a28
chdanish <danish.a.chaudhry@gmail.com>
2016-03-10 17:18:23 +0500

    added frined request relationship entity

---
+++ domain/FriendRequestRelationship.java
@@ -0,0 +1,14 @@
+package so.pickme.replica.domain;
+
+public class FriendRequestRelationship extends BaseRelationship {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
