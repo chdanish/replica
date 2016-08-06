5c1e069d6a09ae05f58620c85cad5ad5adc595c8
chdanish <danish.a.chaudhry@gmail.com>
2016-08-06 10:56:36 +0500

    final commit from win7 authserv

--- config/NeoConfigSDN4.java
+++ config/NeoConfigSDN4.java
@@ -13,13 +13,16 @@ public class NeoConfigSDN4 extends Neo4jConfiguration {
 
 	@Override
 	public SessionFactory getSessionFactory() {
+		//return new SessionFactory("so.pickme.replica.domain");
 		return new SessionFactory(getConfiguration(), "so.pickme.replica.domain");
 	}
 
 	@Bean
 	public org.neo4j.ogm.config.Configuration getConfiguration() {
 		org.neo4j.ogm.config.Configuration config = new org.neo4j.ogm.config.Configuration();
-		config.driverConfiguration().setDriverClassName("org.neo4j.ogm.drivers.http.driver.HttpDriver").setURI("http://neo4j:neo4j2319@neo4j-pickme.rhcloud.com");
+		config.driverConfiguration()
+		.setDriverClassName("org.neo4j.ogm.drivers.http.driver.HttpDriver")
+		.setURI("http://neo4j:neo4j2319@neo-pickme.rhcloud.com");
 		return config;
 	}
 
